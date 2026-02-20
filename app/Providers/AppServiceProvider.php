f370a9086aee1d0393aa07bbdbe79de3d7d453ad
aptikasijunjung-crypto <aptikasijunjung@gmail.com>
2026-02-20 18:17:27 +0700

    deploy-sppt

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Paginator::useBootstrapFive();
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
